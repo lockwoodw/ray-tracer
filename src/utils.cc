7d482e4258610564fd96cc5b36f15c68ee354502
William Lockwood <byffen@gmail.com>
2025-08-10 14:21:59 -0400

    Include check for infinity in floating_point_compare()

--- src/utils.cc
+++ src/utils.cc
@@ -7,19 +7,25 @@
 // see https://www.learncpp.com/cpp-tutorial/relational-operators-and-floating-point-comparisons/
 
 bool floating_point_relative_compare(double a, double b, double rel_epsilon) {
-    return (std::abs(a - b) <= (std::max(std::abs(a), std::abs(b)) * rel_epsilon));
+    return (std::fabs(a - b) <= (std::max(std::fabs(a), std::fabs(b)) * rel_epsilon));
 }
 
 bool floating_point_compare(double a, double b, double abs_epsilon, double rel_epsilon) {
+    // infinity check: normal floating-point comparisons don't seem to apply
+    // in this case; regular equality check will catch if both values are infinite;
+    // otherwise, they are always not equal
+    if (std::isinf(a) || std::isinf(b)) {
+        return a == b;
+    }
     // if the numbers are close, use the regular epsilon method
-    if (std::abs(a - b) <= abs_epsilon)
+    if (std::fabs(a - b) <= abs_epsilon)
         return true;
     // otherwise use Knuth's scaled approach
     return floating_point_relative_compare(a, b, rel_epsilon);
 }
 
 bool simple_floating_point_compare(double a, double b, double epsilon) {
-    return std::abs(a - b) < epsilon;
+    return std::fabs(a - b) < epsilon;
 }
 
 bool FloatingPointComparator::compare(double a, double b) {
